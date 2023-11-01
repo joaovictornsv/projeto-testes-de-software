13000f6f194020382bd48ca1871c9323a1042be9
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-10-31 22:44:36 -0300

    - adds addDetails and calculateAmount methods to Appointment class - adds generateNewProcedure function

--- classes/Appointment.js
+++ classes/Appointment.js
@@ -1,6 +1,11 @@
 // Consulta
 import { generateRandomId } from '../utils/utils.js';
 
+export const generateNewProcedure = (procedure, amount) => ({
+  procedure,
+  amount,
+});
+
 export class Appointment {
   #id;
   #patientId;
@@ -15,5 +20,17 @@ export class Appointment {
     this.#dentistId = appointmentData.dentistId;
     this.#patientId = appointmentData.patientId;
     this.#type = appointmentData.type;
+  }
+
+  addDetails(appointmentDetails) {
+    this.#details = appointmentDetails.details;
+    this.#procedures = appointmentDetails.procedures;
+  }
+
+  calculateAmount() {
+    this.#amount = this.#procedures.reduce(
+      (acc, procedure) => acc + procedure.amount,
+      0,
+    );
   }
 }
